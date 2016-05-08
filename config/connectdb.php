a801672c81c3252dc39ea839bdc2d43dc1950ea0
huynhducphuc <huynhducphucdn94@gmail.com>
2016-05-08 09:12:58 +0700

    set utf-8 cho db

--- config/connectdb.php
+++ config/connectdb.php
@@ -7,5 +7,6 @@
 
     $link = mysql_connect("localhost","root","") or die("Khong the ket noi!");
     mysql_select_db("qlthiolympicdb",$link);
+    mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $link);
 
 ?>
